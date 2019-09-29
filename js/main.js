c847a38d9dfb56cbccaf4a16684d76ec9e4a0904
Jirawat Srisawat <izenguardmax@gmail.com>
2019-09-30 01:55:21 +0700

    ...

--- js/main.js
+++ js/main.js
@@ -2,35 +2,50 @@
     	$( document ).ready( function() {
 
             var entries = [
-                { label: 'Java Script', url: 'http://niklasknaack.blogspot.de/', target: '_top' },
-                { label: 'Java (Android)', url: 'http://www.flashforum.de/', target: '_top' },
-                { label: 'C#', url: 'http://www.jqueryscript.net/', target: '_top' },
-                { label: 'ASP.Net', url: 'http://forum.jswelt.de/', target: '_top' },
-                { label: 'Sql Server', url: 'https://jsfiddle.net/user/NiklasKnaack/fiddles/', target: '_top' },
-                { label: 'My Sql', url: 'http://jquery-plugins.net/', target: '_top' },
-                { label: 'HTML', url: 'http://codepen.io/', target: '_top' },
-                { label: 'CSS', url: 'http://threejs.org/', target: '_top' },
-                { label: 'IONIC', url: 'http://webglstudio.org/', target: '_top' },
-                { label: 'NODE JS', url: 'http://jscompress.com/', target: '_top' },
-                { label: 'Cordova', url: 'https://tinypng.com/', target: '_top' },
-                { label: 'Gradle', url: 'http://caniuse.com/', target: '_top' },
-                { label: 'Visual Studio', url: 'https://goo.gl/', target: '_top' },
-                { label: 'CodePen', url: 'http://www.opinionatedgeek.com/DotNet/Tools/HTMLEncode/Encode.aspx', target: '_top' },
-                { label: 'JSFiddle', url: 'https://twitter.com/niklaswebdev', target: '_top' },
-                { label: 'Stack OverFlow', url: 'http://nkunited.deviantart.com/', target: '_top' },
-                { label: 'Angular JS', url: 'http://gulpjs.com/', target: '_top' },
-                { label: 'GitHub', url: 'https://github.com/', target: '_top' },
-                { label: 'DroidSans', url: 'https://www.shadertoy.com/', target: '_top' },
-                { label: 'Medium.com', url: 'http://gamua.com/starling/', target: '_top' },
-                { label: 'Test Flight', url: 'http://jsperf.com/', target: '_top' },
-                { label: 'Generate qrCode', url: 'http://foundation.zurb.com/', target: '_top' },
-                { label: 'Chrome debug', url: 'http://createjs.com/', target: '_top' },
-                { label: 'Geolocation', url: 'http://julian.com/research/velocity/', target: '_top' },
-                { label: 'Calendar', url: 'https://greensock.com/docs/#/HTML5/GSAP/TweenLite/', target: '_top' },
-                { label: 'Chart', url: 'https://jquery.com/', target: '_top' },
-                { label: 'KnockOut', url: 'http://www.jqueryrain.com/', target: '_top' },
-                { label: 'Organization Chart', url: 'http://jquery-plugins.net/', target: '_top' },
-                { label: 'Bootstrap', url: 'http://jquery-plugins.net/', target: '_top' }
+                { label: 'Java Script', url: '', target: '_top', tooltip: 'Programming Language' },
+                { label: 'Jquery', url: '', target: '_top', tooltip: 'Programming Language' },
+                { label: 'Java (Android)', url: 'http://www.flashforum.de/', target: '_top', tooltip: 'Programming Language' },
+                { label: 'C#', url: 'http://www.jqueryscript.net/', target: '_top' , tooltip: 'Programming Language'},
+                { label: 'ASP.Net', url: 'http://forum.jswelt.de/', target: '_top' , tooltip: 'Programming Language'},
+                { label: 'HTML', url: 'http://codepen.io/', target: '_top', tooltip: 'Programming Language' },
+                { label: 'CSS', url: 'http://threejs.org/', target: '_top' , tooltip: 'Programming Language'},
+                { label: 'Angular JS', url: 'http://gulpjs.com/', target: '_top', tooltip: 'Programming Language' },
+                { label: 'SQL', url: 'http://gulpjs.com/', target: '_top', tooltip: 'Programming Language' },
+                { label: 'KnockOut', url: 'http://www.jqueryrain.com/', target: '_top' , tooltip: 'Programming Language' },
+
+                { label: 'Sql Server', url: 'https://jsfiddle.net/user/NiklasKnaack/fiddles/', target: '_top' , tooltip: 'Programming tool' },
+                { label: 'My Sql', url: 'http://jquery-plugins.net/', target: '_top' , tooltip: 'Programming tool' },
+                { label: 'Visual Studio', url: 'https://goo.gl/', target: '_top' , tooltip: 'Programming tool' },
+                { label: 'CodePen', url: 'http://www.opinionatedgeek.com/DotNet/Tools/HTMLEncode/Encode.aspx', target: '_top' , tooltip: 'Programming tool' },
+                { label: 'JSFiddle', url: 'https://twitter.com/niklaswebdev', target: '_top' , tooltip: 'Programming tool' },
+                { label: 'Chrome debug', url: 'http://createjs.com/', target: '_top' , tooltip: 'Programming tool' },
+
+                { label: 'Test Flight', url: 'http://jsperf.com/', target: '_top' , tooltip: 'Test tool' },
+
+                { label: 'IONIC', url: 'http://webglstudio.org/', target: '_top' , tooltip: 'Framework' },
+                { label: 'Bootstrap', url: 'http://jquery-plugins.net/', target: '_top' , tooltip: 'Framework' },
+
+                { label: 'Node.js', url: 'http://jscompress.com/', target: '_top' , tooltip: 'Platform' },
+                { label: 'Cordova', url: 'https://tinypng.com/', target: '_top' , tooltip: 'Platform' },
+
+                { label: 'Gradle', url: 'http://caniuse.com/', target: '_top' , tooltip: 'build tools' },
+
+                { label: 'Stack OverFlow', url: 'http://nkunited.deviantart.com/', target: '_top' , tooltip: 'web of knowledge' },
+                { label: 'DroidSans', url: 'https://www.shadertoy.com/', target: '_top' , tooltip: 'web of knowledge' },
+                { label: 'Medium.com', url: 'http://gamua.com/starling/', target: '_top' , tooltip: 'web of knowledge' },
+                { label: 'google', url: 'http://gamua.com/starling/', target: '_top' , tooltip: 'web of knowledge' },
+
+                { label: 'GitHub', url: 'https://github.com/', target: '_top' , tooltip: 'Version control' },
+                { label: 'Firebase', url: 'https://github.com/', target: '_top' , tooltip: 'Push Notification' },
+
+                { label: 'Generate qrCode', url: 'http://foundation.zurb.com/', target: '_top' , tooltip: 'library' },
+                { label: 'd3js', url: 'http://foundation.zurb.com/', target: '_top' , tooltip: 'library' },
+                { label: 'Geolocation', url: 'http://julian.com/research/velocity/', target: '_top' , tooltip: 'library' },
+                { label: 'Calendar', url: 'https://greensock.com/docs/#/HTML5/GSAP/TweenLite/', target: '_top' , tooltip: 'library' },
+                { label: 'Chart', url: 'https://jquery.com/', target: '_top' , tooltip: 'library' },
+                { label: 'Organization Chart', url: 'http://jquery-plugins.net/', target: '_top' , tooltip: 'library' },
+
+                { label: 'Stored procedure', url: 'http://gulpjs.com/', target: '_top', tooltip: '' }
             ];
 
             var settings = {
@@ -49,10 +64,21 @@
                 fontFamily: 'Oswald, Arial, sans-serif',
                 fontSize: '32',
                 fontColor: '#fff',
-                fontWeight: 'bold',//bold
+                fontWeight: 'normal',//bold
                 fontStyle: 'normal',//italic 
                 fontStretch: 'normal',//wider, narrower, ultra-condensed, extra-condensed, condensed, semi-condensed, semi-expanded, expanded, extra-expanded, ultra-expanded
-                fontToUpperCase: true
+                fontToUpperCase: true,
+                tooltipFontFamily: 'Oswald, Arial, sans-serif',
+                tooltipFontSize: '16',
+                tooltipFontColor: '#fff',
+                tooltipFontWeight: 'normal',//bold
+                tooltipFontStyle: 'normal',//italic 
+                tooltipFontStretch: 'normal',//wider, narrower, ultra-condensed, extra-condensed, condensed, semi-condensed, semi-expanded, expanded, extra-expanded, ultra-expanded
+                tooltipFontToUpperCase: false,
+                tooltipTextAnchor: 'middle',
+                tooltipDiffX: 0,
+                tooltipDiffY: 35
+
             };
 
             //var svg3DTagCloud = new SVG3DTagCloud( document.getElementById( 'holder'  ), settings );
